07d3d9834f9da7dfe8be7684a8fb09dec3c46b80
The-Sireatsalot <135057072+The-Sireatsalot@users.noreply.github.com>
2025-09-22 18:19:28 -0700

    fixed block jumping and crouching

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758588796|1459717835';
+const CACHE_VERSION = '1758590317|480075873';
 /** @type {string} */
 const CACHE_PREFIX = 'learning worlds-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
