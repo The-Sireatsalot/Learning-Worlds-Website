45ce7f89f8db72aceec0bdea04d42b2e3071a24f
The-Sireatsalot <135057072+The-Sireatsalot@users.noreply.github.com>
2025-09-26 22:13:17 -0700

    Added player

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758943084|1842269120';
+const CACHE_VERSION = '1758949978|116800168';
 /** @type {string} */
 const CACHE_PREFIX = 'learning worlds-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
