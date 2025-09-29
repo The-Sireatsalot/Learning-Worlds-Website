2bcc4e7bb0bcd367a3e01067eba6d8ab65040ac0
The-Sireatsalot <135057072+The-Sireatsalot@users.noreply.github.com>
2025-09-29 10:16:06 -0700

    updated website

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759128034|6006431984';
+const CACHE_VERSION = '1759166113|68075475';
 /** @type {string} */
 const CACHE_PREFIX = 'learning worlds-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
