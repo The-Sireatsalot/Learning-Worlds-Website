65e6bac7f58e26dd28b1bc31c96f3922e3d5631c
The-Sireatsalot <135057072+The-Sireatsalot@users.noreply.github.com>
2025-10-09 15:31:35 -0700

    Added header data

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1760047721|1525524339';
+const CACHE_VERSION = '1760049048|2851646438';
 /** @type {string} */
 const CACHE_PREFIX = 'learning worlds-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
